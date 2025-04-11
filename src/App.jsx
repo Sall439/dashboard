95748ac3c123a351e656096b8cb378a75b4adb71
Baba Faye <fayebaba81@gmail.com>
2025-04-11 15:56:51 +0000

    recuperation des donnes et redopsition

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
 import Main from "./components/Main"
-import "animate.css"
+// import "animate.css"
 import './App.css'
 
 function App() {
