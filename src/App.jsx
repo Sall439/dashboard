a3631efb2dd6b298a396bfa28755260168873d1a
Baba Faye <fayebaba81@gmail.com>
2025-04-11 23:36:20 +0000

    commit1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@
 import './App.css'
 
 import { ContentBoard } from "./Admin/ContentBoard"
+import { Inscription } from './Admin/Inscription/Inscription'
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -11,8 +12,8 @@ function App() {
 
   return (
       <div className="app">
-       {/* <Inscription/> */}
-       <ContentBoard/>
+       <Inscription/>
+       {/* <ContentBoard/> */}
       </div>
   )
 }
