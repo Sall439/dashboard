31bca51abb84df04fcbb77b46172475a7c63ee5d
Baba Faye <fayebaba81@gmail.com>
2025-04-15 22:02:35 +0000

    vider app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,11 @@ import { ContentBoard } from "./Admin/ContentBoard"
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  
-  const [livraison, setLivraison] = useState([])
 
-  const handleAddLivraison = (newLiv) => {
-    setLivraison(prev => [newLiv, ...prev]) 
-  }
 
   return (
       <div className="app">
-       {/* <Inscription/> */}
-       <ContentBoard/>
+     
       </div>
   )
 }
