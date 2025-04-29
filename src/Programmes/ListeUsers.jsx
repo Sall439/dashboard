f0e20f31f976c015926c9fcee43fa77764efb943
Baba Faye <fayebaba81@gmail.com>
2025-04-29 15:10:51 +0000

    ajout de filter et de supprimer sur la liste users

--- src/Programmes/ListeUsers.jsx
+++ src/Programmes/ListeUsers.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
 import { useContextInscription } from "../Admin/useContext/UseInscription";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 export const ListeUsers = () => {
     const [users, setUsers] = useState([])
+    const [filter,setFilter] = useState("")
   const {url} = useContextInscription()
+
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -17,9 +21,13 @@ export const ListeUsers = () => {
                 console.error("Erreur de chargement des données :", err);
             }
         };
-
         fetchData();
     },[])
+
+    console.log(users);
+    
+// filtrage des utilisateurs
+    const filteredUsers = users.filter((user)=>user.nom.toLowerCase().includes(filter.toLowerCase()))
 
     const handleRoleChange = async (userId, newRole) => {
         const token = localStorage.getItem("token");
@@ -38,9 +46,32 @@ export const ListeUsers = () => {
           );
         } catch (err) {
           console.error("Erreur lors de la mise à jour du role :", err);
+          alert("Une erreur s'est produite lors de la mise à jour du role.");
         }
+
+        {filteredUsers.length === 0 && alert("Aucun utilisateur trouve")}
       };
-    return <div className="p-8 w-full flex justify-center items-center">
+
+      // suppression des utilisateurs
+      const handleDeleteUser = async (userId) => {
+        const token = localStorage.getItem("token");
+        try {
+          await axios.delete(`${url}/admin/users/${userId}`, {
+            headers: { Authorization: `Bearer ${token}` }
+          });
+    
+          // mettre à jour localement
+          setUsers((prev) => prev.filter((user) => user.id !== userId));
+        } catch (err) {
+          console.error("Erreur lors de la suppression de l'utilisateur :", err);
+          alert("Une erreur s'est produite lors de la suppression de l'utilisateur.");
+        }
+      }
+    return <div className="p-8 w-full flex justify-center items-center flex-col gap-3">
+      <div className="filter text-center w-[90%] mb-3">
+        <input type="search" placeholder="Rechercher un utilisateurs..." className="border-2 w-full p-2 rounded-2xl outline-0"
+         value={filter} onChange={(e) => setFilter(e.target.value)}/>
+      </div>
         <table className="w-full border border-collapse">
         <thead>
             <tr>
@@ -49,10 +80,11 @@ export const ListeUsers = () => {
                 <th className="border border-collapse p-2">Prenom</th>
                 <th className="border border-collapse p-2">Email</th>
                 <th className="border border-collapse p-2">Role</th>
+                <th className="border border-collapse p-2">Actions</th>
             </tr>
         </thead>
         <tbody>
-         {users.map((user,index)=>{
+         {filteredUsers.map((user,index)=>{
             return <tr key={user.id}>
                 <td className="border border-collapse p-2 truncate max-w-[100px] sm:max-w-[200px] md:max-w-[300px]">{index+1}</td>
                 <td className="border border-collapse p-2 truncate max-w-[100px] sm:max-w-[200px] md:max-w-[300px]">{user.nom}</td>
@@ -68,6 +100,11 @@ export const ListeUsers = () => {
                   <option value="user" className="text-black">user</option>
                   <option value="admin" className="text-black">admin</option>
                 </select>
+                 </td>
+
+                 <td className="border border-collapse p-2 truncate max-w-[100px] sm:max-w-[200px] md:max-w-[300px] flex items-center justify-center">
+                  <button className="text-2xl cursor-pointer" title="supprimer" onClick={() => handleDeleteUser(user.id)}><FaTrash /></button>
+                  {/* <button className="text-2xl cursor-pointer" title="modifier"><FaEdit /></button> */}
                  </td>
             </tr>
          })}
